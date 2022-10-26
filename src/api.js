4ce3d756f374701ccc7f07a0bf62d0e0a268986f
Nitin-Teja <ni3dasari@gmail.com>
2022-10-26 17:21:08 +0530

    Weather Application

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '39de73751bmshe8a319c069c0ce5p1b3094jsnc1cc7a23f00a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7577d2469ab61f3e5d20c2e99180669d";
